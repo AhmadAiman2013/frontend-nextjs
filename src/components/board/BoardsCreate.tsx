7cdc248c1ae2b1268cf0f636118bc3b9f6daee48
ahmad aiman <ahmadaimanalias@gmai.com>
2024-08-01 17:51:01 +0800

    add create board (1)

---
+++ src/components/board/BoardsCreate.tsx
@@ -0,0 +1,11 @@
+
+
+const BoardsCreate = () => {
+  return (
+    <div className="rounded-lg p-2 flex  justify-center items-center bg-gradient-to-r  from-blue-200 to-fuchsia-200 hover:from-blue-400 hover:to-fuchsia-400  text-blue-800 hover:text-blue-900 dark:text-white/90 dark:bg-gradient-to-r dark:from-blue-500/40 dark:to-fuchsia-500/40 dark:hover:text-white dark:hover:from-blue-500 dark:hover:to-fuchsia-500 duration-300 ease-out hover:scale-110">
+        Write one!
+    </div>
+  )
+}
+
+export default BoardsCreate
---
+++ src/components/board/BoardsPopover.tsx
@@ -0,0 +1,13 @@
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "@/components/ui/popover"
+
+const BoardsPopover = () => {
+  return (
+    <div>BoardsPopover</div>
+  )
+}
+
+export default BoardsPopover
