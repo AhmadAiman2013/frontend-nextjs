981b550c4b6e8fb55c1e137773752d2d3ba0cec5
ahmad aiman <ahmadaimanalias@gmail.com>
2024-08-16 23:45:46 +0800

    changes hooks for task

--- src/hooks/useTask.ts
+++ src/hooks/useTask.ts
@@ -17,135 +17,169 @@ export const useTask = ({ boardId, id }: TaskProps) => {
   const { isEditingTask, startEditingTask, stopEditingTask } = useEditStore(
     (state) => state
   );
-  
 
-  const { mutateAsync: createTaksMutation, isPending : isPendingCreate } = useMutation({
-    mutationFn: async (data: TaskData) => {
-      const response = await axios.post<{ data: TaskType }>(
-        `/api/cards/${data.cardId}/tasks`,
-        data
-      );
-      return { data: response.data.data };
-    },
-    onSuccess: (newTask) => {
-      queryClient.setQueryData(
-        ["board", boardId],
-        (oldData: { data: BoardIdType }) => {
-          if (!oldData)
-            return {
-              data: {
-                cards: [{ id: newTask.data.card_id, tasks: [newTask.data] }],
-              },
-            };
-          const newCards = oldData.data.cards.map((card) => {
-            if (card.id === newTask.data.card_id) {
-              return { ...card, tasks: [...card.tasks, newTask.data] };
-            }
-            return card;
-          });
-          return { data: { cards: newCards } };
-        }
-      );
-    },
-  });
+  const { mutateAsync: createTaksMutation, isPending: isPendingCreate } =
+    useMutation({
+      mutationFn: async (data: TaskData) => {
+        const response = await axios.post<{ data: TaskType }>(
+          `/api/cards/${data.cardId}/tasks`,
+          data
+        );
+        return { data: response.data.data };
+      },
+      onSuccess: (newTask) => {
+        queryClient.setQueryData(
+          ["board", boardId],
+          (oldData: { data: BoardIdType }) => {
+            if (!oldData)
+              return {
+                data: {
+                  cards: [{ id: newTask.data.card_id, tasks: [newTask.data] }],
+                },
+              };
+            const newCards = oldData.data.cards.map((card) => {
+              if (card.id === newTask.data.card_id) {
+                return { ...card, tasks: [...card.tasks, newTask.data] };
+              }
+              return card;
+            });
+            return { data: { cards: newCards } };
+          }
+        );
+      },
+    });
 
   // create task
   const createCard = async (data: TaskData): Promise<TaskResponse> => {
     try {
-        return await createTaksMutation(data);
+      return await createTaksMutation(data);
     } catch (error) {
-        console.error("create task failed");
-        if (error instanceof AxiosError && error.response) {
-            return {
-            error: error.response.data.message || "An unexpected error occurred",
-            };
-        }
+      console.error("create task failed");
+      if (error instanceof AxiosError && error.response) {
         return {
-            error: "An unexpected error occurred",
+          error: error.response.data.message || "An unexpected error occurred",
         };
+      }
+      return {
+        error: "An unexpected error occurred",
+      };
     }
-  }
+  };
 
   // update task mutation
-  const { mutateAsync: updateTaskMutation, isPending : isPendingUpdate } = useMutation({
-    mutationFn: async (data: TaskData) => {
-        const response = await axios.put<{data : TaskType}>(`/api/cards/${data.cardId}/tasks/${id}`, data);
-        return {data: response.data.data};
-    },
-    onSuccess: (newTask) => {
-        queryClient.setQueryData(["board", boardId], (oldData : {data : BoardIdType}) => {
-            if (!oldData) return { data: { cards: [{ id: newTask.data.card_id, tasks: [newTask.data] }] } };
+  const { mutateAsync: updateTaskMutation, isPending: isPendingUpdate } =
+    useMutation({
+      mutationFn: async (data: TaskData) => {
+        const response = await axios.put<{ data: TaskType }>(
+          `/api/cards/${data.cardId}/tasks/${id}`,
+          data
+        );
+        return { data: response.data.data };
+      },
+      onSuccess: (newTask) => {
+        queryClient.setQueryData(
+          ["board", boardId],
+          (oldData: { data: BoardIdType }) => {
+            if (!oldData)
+              return {
+                data: {
+                  cards: [{ id: newTask.data.card_id, tasks: [newTask.data] }],
+                },
+              };
             const newCards = oldData.data.cards.map((card) => {
-                if (card.id === newTask.data.card_id) {
-                    return { ...card, tasks: card.tasks.map((task) => {
-                        if (task.id === id) {
-                            return newTask.data;
-                        }
-                        return task;
-                    }) };
-                }
-                return card;
+              if (card.id === newTask.data.card_id) {
+                return {
+                  ...card,
+                  tasks: card.tasks.map((task) => {
+                    if (task.id === id) {
+                      return newTask.data;
+                    }
+                    return task;
+                  }),
+                };
+              }
+              return card;
             });
             return { data: { cards: newCards } };
-        });
-    }
-  })
+          }
+        );
+      },
+    });
   // update task
   const updateTask = async (data: TaskData): Promise<TaskResponse> => {
     try {
-        return await updateTaskMutation(data);
+      return await updateTaskMutation(data);
     } catch (error) {
-        console.error("update task failed");
-        if (error instanceof AxiosError && error.response) {
-            return {
-            error: error.response.data.message || "An unexpected error occurred",
-            };
-        }
+      console.error("update task failed");
+      if (error instanceof AxiosError && error.response) {
         return {
-            error: "An unexpected error occurred",
+          error: error.response.data.message || "An unexpected error occurred",
         };
+      }
+      return {
+        error: "An unexpected error occurred",
+      };
     }
-  }
+  };
 
   // delete task mutation
-  const { mutateAsync: deleteTaskMutation, isPending: isPendingDelete } = useMutation({
-    mutationFn: async (cardId: Pick<TaskData, "cardId">) => {
+  const { mutateAsync: deleteTaskMutation, isPending: isPendingDelete } =
+    useMutation({
+      mutationFn: async (cardId: Pick<TaskData, "cardId">) => {
         await axios.delete(`/api/cards/${cardId}/tasks/${id}`);
-    },
-    onSuccess: (_, variables) => {
-        queryClient.setQueryData(["board", boardId], (oldData : {data : BoardIdType}) => {
-            if (!oldData) return ;
+      },
+      onSuccess: (_, variables) => {
+        queryClient.setQueryData(
+          ["board", boardId],
+          (oldData: { data: BoardIdType }) => {
+            if (!oldData) return;
+            const deletedTaskOrder =
+              oldData.data.cards
+                .find((card) => card.id === variables.cardId)
+                ?.tasks.find((task) => task.id === id)?.order || 0;
             return {
-                data: {
-                    ...oldData.data,
-                    cards: oldData.data.cards.map((card) => {
-                        if (card.id === variables.cardId) {
-                            return { ...card, tasks: card.tasks.filter((task) => task.id !== id) };
-                        }
-                        return card;
-                    }),
-                }
-            }
-        });
-    }
-  })
+              data: {
+                ...oldData.data,
+                cards: oldData.data.cards.map((card) => {
+                  if (card.id === variables.cardId) {
+                    return {
+                      ...card,
+                      tasks: card.tasks
+                        .filter((task) => task.id !== id)
+                        .map((task) => ({
+                          ...task,
+                          order:
+                            task.order > deletedTaskOrder
+                              ? task.order - 1
+                              : task.order,
+                        })),
+                    };
+                  }
+                  return card;
+                }),
+              },
+            };
+          }
+        );
+      },
+    });
 
   // delete task
-    const deleteTask = async (cardId: Pick<TaskData, "cardId">) => {
-        try {
-            await deleteTaskMutation(cardId);
-        } catch (error) {
-            console.error("delete task failed");
-            if (error instanceof AxiosError && error.response) {
-                return {
-                error: error.response.data.message || "An unexpected error occurred",
-                };
-            }
-            return {
-                error: "An unexpected error occurred",
-            };
-        }
+  const deleteTask = async (cardId: Pick<TaskData, "cardId">) => {
+    try {
+      await deleteTaskMutation(cardId);
+    } catch (error) {
+      console.error("delete task failed");
+      if (error instanceof AxiosError && error.response) {
+        return {
+          error: error.response.data.message || "An unexpected error occurred",
+        };
+      }
+      return {
+        error: "An unexpected error occurred",
+      };
     }
+  };
 
   return {
     createCard,
@@ -157,5 +191,5 @@ export const useTask = ({ boardId, id }: TaskProps) => {
     startEditingTask,
     stopEditingTask,
     isEditingTask,
-  }
+  };
 };
--- src/types/schema/TaskSchema.ts
+++ src/types/schema/TaskSchema.ts
@@ -3,7 +3,6 @@ import * as v from 'valibot';
 export const TaskDataSchema = v.object({
     cardId: v.pipe(v.string(), v.nonEmpty('Card ID is required')),
     title: v.pipe(v.string(), v.nonEmpty('Title is required'), v.maxLength(50, 'The title must not exceed 50 characters.')),
-    order: v.pipe(v.number(), v.minValue(1, 'Order must be greater than or equal to 1')),
     completed: v.optional(v.boolean('Completed must be a boolean')),
 })
 
