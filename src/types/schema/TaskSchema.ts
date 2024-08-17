3263e15a4d399458a27620082b0737e7b62ebb4d
ahmad aiman <ahmadaimanalias@gmail.com>
2024-08-17 15:57:34 +0800

    add due_date schema

--- src/types/schema/TaskSchema.ts
+++ src/types/schema/TaskSchema.ts
@@ -4,6 +4,7 @@ export const TaskDataSchema = v.object({
     cardId: v.pipe(v.string(), v.nonEmpty('Card ID is required')),
     title: v.pipe(v.string(), v.nonEmpty('Title is required'), v.maxLength(50, 'The title must not exceed 50 characters.')),
     completed: v.optional(v.boolean('Completed must be a boolean')),
+    due_date: v.optional(v.date('Due date must be a date')),
 })
 
 export type TaskData = v.InferOutput<typeof TaskDataSchema>;
