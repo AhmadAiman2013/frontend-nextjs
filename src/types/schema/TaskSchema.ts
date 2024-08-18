2a03b00d1ed8a088f5246898570c9ec1aec62859
ahmad aiman <ahmadaimanalias@gmail.com>
2024-08-18 18:34:52 +0800

    changes task schema

--- src/types/schema/TaskSchema.ts
+++ src/types/schema/TaskSchema.ts
@@ -3,8 +3,16 @@ import * as v from 'valibot';
 export const TaskDataSchema = v.object({
     cardId: v.pipe(v.string(), v.nonEmpty('Card ID is required')),
     title: v.pipe(v.string(), v.nonEmpty('Title is required'), v.maxLength(50, 'The title must not exceed 50 characters.')),
-    completed: v.optional(v.boolean('Completed must be a boolean')),
-    due_date: v.optional(v.date('Due date must be a date')),
+})
+
+export const TaskEditDataSchema = v.object({
+    ...v.partial(TaskDataSchema, ['title']).entries,
+    ...v.object({
+        completed: v.optional(v.boolean('Completed must be a boolean')),
+        due_date: v.optional(v.date('Due date must be a date')),
+    }).entries
 })
 
 export type TaskData = v.InferOutput<typeof TaskDataSchema>;
+
+export type TaskEditData = v.InferOutput<typeof TaskEditDataSchema>;
