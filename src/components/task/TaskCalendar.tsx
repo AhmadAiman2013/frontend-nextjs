19b8709f3f7e88827bb56efd3b60e82b0479fe0b
ahmad aiman <ahmadaimanalias@gmail.com>
2024-08-26 16:22:05 +0800

    adjust button for task calendar

--- src/components/task/TaskCalendar.tsx
+++ src/components/task/TaskCalendar.tsx
@@ -3,29 +3,34 @@ import PopoverElement from "../PopoverElement";
 import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
 import { useState } from "react";
-import dayjs from 'dayjs';
-import localizedFormat from 'dayjs/plugin/localizedFormat'
-
+import { useTask } from "@/hooks/useTask";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
 
 const TaskCalendar = () => {
   const [date, setDate] = useState<Date>();
-  dayjs.extend(localizedFormat)
-
+  const { updateTask, isPendingUpdate } = useTask({});
+  dayjs.extend(localizedFormat);
   return (
     <PopoverElement
       triggerElement={
-        <Button size="icon" variant="ghost">
+        <Button size="icon" variant="ghost" disabled={isPendingUpdate}>
           <CalendarPlus size={20} />
         </Button>
       }
       popoverContent={
         <div>
-          <Calendar
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            initialFocus
-          />
+          <div>
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              initialFocus
+            />
+          </div>
+          <div className="mt-1">
+            <Button className="w-full bg-gradient-to-r  from-blue-200 to-fuchsia-200 hover:from-blue-400 hover:to-fuchsia-400  text-blue-800 hover:text-blue-900 dark:text-white/90 dark:bg-gradient-to-r dark:from-blue-500/40 dark:to-fuchsia-500/40 dark:hover:text-white dark:hover:from-blue-500 dark:hover:to-fuchsia-500">Set Date</Button>
+          </div>
         </div>
       }
       side="right"
